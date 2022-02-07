83f034b95d6ae03c2093e143d55167109f0f1965
Leo Joseph <leojoseph.r@rakuten.com>
2022-02-07 12:43:35 +0900

    Moved to actions

--- scripts/generate-coverage.sh
+++ scripts/generate-coverage.sh
@@ -2,6 +2,7 @@
 
 pod install
 bundle install
+echo "Generate Coverage Script ****"
 /usr/libexec/PlistBuddy -c "Set :RATEndpoint ${{env.RATEndpoint}}" Example/Info.plist
 /usr/libexec/PlistBuddy -c "Set :RMAAPIEndpoint $env.RMAAPIEndpoint" Example/Info.plist
 bundle exec fastlane code_coverage
