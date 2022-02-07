b5217d72a4be1e1b55c0b5e5f59a1e914ff79e44
Leo Joseph <leojoseph.r@rakuten.com>
2022-02-07 14:03:05 +0900

    Refactor

--- scripts/gen-cov.sh
+++ scripts/gen-cov.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-pod install
-bundle install
-bundle exec fastlane code_coverage
+# pod install
+# bundle install
+# bundle exec fastlane code_coverage
+/usr/libexec/PlistBuddy -c 'print RATEndpoint' Example/Info.plist
+/usr/libexec/PlistBuddy -c 'print RATEndpoint' /Example/Info.plist
+
